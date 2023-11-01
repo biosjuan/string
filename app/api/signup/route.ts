5779cf429dab34c4e3e0c7a84f5104155dbc9d0e
Hyuk Lee <hyuklee@hyuks-mbp.mynetworksettings.com>
2023-11-01 09:57:21 -0700

    added sign in route

---
+++ app/api/signup/route.ts
@@ -0,0 +1,28 @@
+import { sql } from "@/db";
+import bcrypt from "bcrypt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const json = await request.json();
+
+  const res = await sql(
+    "select id, username from users where username ilike $1",
+    [json.username]
+  );
+
+  if (res.rowCount > 0)
+    return NextResponse.json({ error: "user already exists" }, { status: 400 });
+
+  const saltRountds = 10;
+  const hash = await bcrypt.hash(json.password, saltRountds);
+
+  await sql("insert into users (username, password) values($1, $2)", [
+    json.username,
+    hash,
+  ]);
+
+  return NextResponse.json(
+    { message: "registration success" },
+    { status: 201 }
+  );
+}
